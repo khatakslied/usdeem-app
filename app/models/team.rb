e2af10b8b80666506d9be26ab7f51bb2edd92f54
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-01-15 23:27:30 +0900

    Add has_many relationships to models

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,4 @@
 class Team < ApplicationRecord
   belongs_to :company
+  has_many :users, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :team
+  has_many :reviews, dependent: :destroy
 end
