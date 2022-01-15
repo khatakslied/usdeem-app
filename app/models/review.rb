e2ffd763c1db7d24df3d21698ebe014002a5c013
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-01-15 23:20:56 +0900

    Refactor review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :sender, :class_name => "User", :foreign_key  => "sender_id"
-  belongs_to :recipient, :class_name => "User", :foreign_key  => "recipient_id"
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id"
+  belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
 end
