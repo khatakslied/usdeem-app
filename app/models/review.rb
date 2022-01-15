8e620f3c43b70b8866f18145ae9e5696ae25e3da
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-01-15 23:35:40 +0900

    Add presence validations to models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
+  validates :teamwork, presence: true
+  validates :communication, presence: true
+  validates :time_management, presence: true
+  validates :problem_solving, presence: true
+  validates :leadership, presence: true
+  validates :work_ethic, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :company
   has_many :users, dependent: :destroy
+  validates :name, presence: true
 end
