bc1452bd027c3470a79d9146797779e7bfb24fd7
matias acuna <agroang@gmail.com>
2022-01-22 14:53:45 +0900

    pages controller, react component and home.html.erb changes

--- app/javascript/components/ProfileMessage.js
+++ app/javascript/components/ProfileMessage.js
@@ -1,16 +1,22 @@
 import React from "react"
-import PropTypes from "prop-types"
-class ProfileMessage extends React.Component {
-  render () {
-    return (
-      <React.Fragment>
-        Username: {this.props.username}
-      </React.Fragment>
-    );
-  }
-}
+// import PropTypes from "prop-types"
+// class ProfileMessage extends React.Component {
+//   render () {
+//     return (
+//       <React.Fragment>
+//         Username: {this.props.username}
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-ProfileMessage.propTypes = {
-  username: PropTypes.string
-};
-export default ProfileMessage
+// ProfileMessage.propTypes = {
+//   username: PropTypes.string
+// };
+// export default ProfileMessage
+
+export default function ProfileMessage(props) {
+  return (
+    <h1>{props.user.first_name} {props.user.last_name}</h1>
+  )
+}
