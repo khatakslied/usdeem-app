d07fe8028e04ace9463535880c78c75e56c0fa59
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-01-27 23:02:59 +0900

    Add trait sub-categories to Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,14 @@ class Review < ApplicationRecord
   validates :problem_solving, presence: true
   validates :leadership, presence: true
   validates :work_ethic, presence: true
+
+  ANALYSIS_SKILLS = %w[Divergent_thinking Experimentation Observation]
+  CREATIVE_THINKING_SKILLS = %w[Artistic_sense Imagination Brainstorming Design_sense Innovation Insight Mind_mapping Lateral_thinking]
+  TROUBLESHOOTING_SKILLS = %w[Troubleshooting Questioning Logical_reasoning Persistence Reframing]
 end
+
+# “{
+# Analysis: {score: 2, Analysis: true, Divergent_thinking: false, Experimenting: false, Observation: true },
+# Creativity: {score: 3, Artistic_sense: true, Imagination: false, Brainstorming: true, Design: true, Design_sense: true, Innovation: true, Insight: true, Inspiration: false, Mind_mapping: true, Lateral_thinking: false},
+# Troubleshooting: {score: 5, Troubleshooting: false, Questioning: false, Logical_reasoning: false, Persistence: false, Reframing: false}
+# }”
