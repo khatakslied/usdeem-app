b97c4d0190d1a06bc0b7cdcbf83a86ade912b5fe
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-01-28 17:05:06 +0900

    Create working param filter method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,8 +8,20 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @score = []
+    @review = Review.new(problem_solving: problem_solving_params)
     raise
+  end
+
+  private
+  def problem_solving_params
+    @analysis_skills = params.require(:problem_solving).permit(:analysis_skills_score, analysis_skills_subset: [])
+    @creative_thinking_skills = params.require(:problem_solving).permit(:creative_thinking_skills_score, creative_thinking_skills_subset: [])
+    @troubleshooting_skills = params.require(:problem_solving).permit(:troubleshooting_skills_score, troubleshooting_skills_subset: [])
+    {
+      analysis_skills: { score: @analysis_skills[:analysis_skills_score], subset: @analysis_skills[:analysis_skills_subset][1..-1] },
+      creative_thinking_skills: { score: @creative_thinking_skills[:creative_thinking_skills_score], subset: @creative_thinking_skills[:creative_thinking_skills_subset][1..-1] },
+      troubleshooting_skills: { score: @troubleshooting_skills[:troubleshooting_skills_score], subset: @troubleshooting_skills[:troubleshooting_skills_subset][1..-1] }
+    }
   end
 end
 
