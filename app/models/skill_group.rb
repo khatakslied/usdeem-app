3277f569de952bbe67fcdb00b3e91ca3857245c7
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-02-01 03:15:24 +0900

    Add categories to SkillGroup model

--- app/models/skill_group.rb
+++ app/models/skill_group.rb
@@ -1,6 +1,13 @@
 class SkillGroup < ApplicationRecord
   belongs_to :key_trait
   has_many :skills
-
-
+  accepts_nested_attributes_for :skills
+  CATEGORIES = {
+    teamwork: ["Conflict Resolution Skills", "Collaborative Skills", "Cultural Awareness", "Empathetic Skills"],
+    problem_solving: ["Analysis Skills", "Creative Thinking Skills", "Troubleshooting Skills"],
+    leadership: ["Team Management Skills", "Project Management Skills", "Interpersonal Skills"],
+    work_ethic: ["Adaptability", "Diligence", "Discipline", "Responsibility"],
+    communication: ["General Communcation Skills", "Negotiation Skills", "Diplomatic Skills"],
+    time_management: ["Planning Skills", "Prioritisation", "Performance"]
+  }
 end
