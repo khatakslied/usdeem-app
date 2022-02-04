9c7cc341924314baa2a6d9aa4de1f6fa64ac2cc6
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-02-04 17:27:51 +0900

    Test if initialize method accepts :score

--- spec/models/skill_group_spec.rb
+++ spec/models/skill_group_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SkillGroup, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'checks if the initialize method accepts :score on creation' do
+    test_skill_group = SkillGroup.new(score: 3)
+    expect(test_skill_group.score).to eq(3)
+  end
 end
