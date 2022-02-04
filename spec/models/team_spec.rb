e7c81502f2b767db299e704f14f83288d5479fdb
matias acuna <agroang@gmail.com>
2022-02-04 18:43:34 +0900

    added the peding tests for team model

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Team, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'belongs_to :company'
+
+  it 'has_many :users, dependent: :destroy'
+
+  it 'validates :name, presence: true'
+
+  it 'has a name'
+
+  it 'has a company id? (same as first?)'
 end
