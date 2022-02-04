a6e9868a378ee4b526b373d29481d832932a9d9e
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2022-02-04 17:40:17 +0900

    Test if skill_group belongs to key_trait

--- spec/models/skill_group_spec.rb
+++ spec/models/skill_group_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe SkillGroup, type: :model do
     test_skill_group = SkillGroup.new(score: 3)
     expect(test_skill_group.score).to eq(3)
   end
+
+  context 'checks if it belongs to :key_trait' do
+    it { should belong_to(:key_trait) }
+  end
+
 end
+
+# it 'checks if the initialize method has :score on creation' do
+# test_skill_group = SkillGroup.new(score: '', category: 'Conflict Resolution Skills')
+# expect(test_skill_group).to_not be_valid
+# test_skill_group.score
+# end
