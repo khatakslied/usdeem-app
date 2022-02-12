8d116a146b2a0f25b348b29e1e99bb444cb61a5f
Vivian Bow <vivianbao1009@gmail.com>
2022-02-12 22:47:17 +0900

    Commented out category from each model

--- app/models/key_trait.rb
+++ app/models/key_trait.rb
@@ -2,5 +2,5 @@ class KeyTrait < ApplicationRecord
   belongs_to :report
   has_many :skill_groups
   accepts_nested_attributes_for :skill_groups
-  CATEGORIES = ["teamwork", "communication", "time management", "problem solving", "leadership", "work ethic"]
+  # CATEGORIES = ["teamwork", "communication", "time management", "problem solving", "leadership", "work ethic"]
 end
