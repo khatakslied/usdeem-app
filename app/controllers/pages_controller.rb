d01eeaf3b3b617a7b7722a4d108ab43f24f257ce
Vivian Bow <vivianbao1009@gmail.com>
2022-02-18 10:39:05 +0900

    Created skill rating component and instance variable of last month reports in page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
+    @last_month_reports = Report.where(recipient: current_user,
+                                       created_at: Date.today.beginning_of_month..Date.today.end_of_month)
   end
 end
