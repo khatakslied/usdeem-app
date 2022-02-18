e504434b48ad31bf4629ddba58276f9d630c9a85
Vivian Bow <vivianbao1009@gmail.com>
2022-02-18 15:32:02 +0900

    Tested sending data from controller to components as json

--- app/javascript/components/KeyTraitScore.js
+++ app/javascript/components/KeyTraitScore.js
@@ -2,10 +2,36 @@ import React from "react"
 import SkillGroupRating from "./SkillGroupRating"
 
 export default function KeyTraitScore(props) {
+  const first_report = props.latest_reports[0]
+  const key_traits = first_report.key_traits.map(key_trait => {
+    return(
+      <p key={key_trait.category}>{key_trait.category}</p>
+    )
+  }
+  )
+
+  const first_key_trait = first_report.key_traits[0]
+  const skill_groups = first_key_trait.skill_groups.map(skill_group => {
+    return (
+      <p key={skill_group.category}>{skill_group.category}</p>
+    )
+  }
+  )
+
+  const first_skill_group = first_key_trait.skill_groups[0]
+
+  const skills = first_skill_group.skills.map(skill => {
+    return(
+      <p key={skill.name}>{skill.name}</p>
+    )
+  })
 
   return(
     <>
       <h2>This is KeyTraitScore component</h2>
+      {key_traits}
+      {skill_groups}
+      {skills}
       <SkillGroupRating />
       <SkillGroupRating />
       <SkillGroupRating />
