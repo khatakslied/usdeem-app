6ea227107edb279bf130c3e588307658c24074de
Konstantin Yatsenko <moahtdeep@gmail.com>
2022-02-18 18:58:40 +0900

    responsive option to a radar component

--- app/javascript/components/Dashboard.js
+++ app/javascript/components/Dashboard.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React from "react";
+import ChartRadar from "./ChartRadar";
 
 const Dashboard = props => {
   return (
     <div className="dashboard-container">
-      <div className="dashboard-column">Left</div>
+      <div className="dashboard-column">
+        <span>Left</span>
+        <ChartRadar />
+      </div>
+
       <div className="dashboard-column">Right</div>
     </div>
   )
