234d1bea59c2fa1c583159f881bf33b7230fc5c1
Vivian Bow <vivianbao1009@gmail.com>
2022-02-18 11:34:16 +0900

    Created new dashboard file, renamed skillrating to skillgroupscore

--- app/javascript/components/Dashboard.js
+++ app/javascript/components/Dashboard.js
@@ -1,12 +1,24 @@
 import React from "react"
+import SkillGroupScore from "./SkillGroupScore"
+import ProfileMessage from "./ProfileMessage"
 
-const Dashboard = props => {
-  return (
-    <div className="dashboard-container">
-      <div className="dashboard-column">Left</div>
-      <div className="dashboard-column">Right</div>
+export default function Dashboard(props) {
+  const [keyTrait, setKeyTrait] = React.useState('Problem Solving')
+
+
+  return(
+    <div>
+      <h1>This is dashboard component</h1>
+      <ProfileMessage user={props.user}/>
+      <div className="dashboard-container">
+        <div className="dashboard-column">
+          <p>Left</p>
+        </div>
+        <div className="dashboard-column">
+          <p>Right</p>
+          <SkillGroupScore latest_reports={props.latest_reports} key_trait={keyTrait}/>
+        </div>
+      </div>
     </div>
   )
-};
-
-export default Dashboard;
+}
---
+++ app/javascript/components/SkillGroupScore.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function SkillRating(props) {
+
+  return(
+    <>
+      <h2>This is skill rating</h2>
+      <div>
+        <h3>Skill_group</h3>
+        <p>Scoring</p>
+        <h4>Aspects to improve:</h4>
+        <div>
+          <p>Skill</p>
+          <p>Skill</p>
+          <p>Skill</p>
+        </div>
+      </div>
+    </>
+  )
+};
