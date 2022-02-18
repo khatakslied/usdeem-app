61c184fd486abe1e057ede618ae6b83777a30100
Vivian Bow <vivianbao1009@gmail.com>
2022-02-18 14:00:27 +0900

    Added basic styling

--- app/javascript/components/SkillGroupRating.js
+++ app/javascript/components/SkillGroupRating.js
@@ -5,13 +5,15 @@ import React from "react"
 export default function SkillGroupRating() {
   return (
     <div>
-      <h3>Skill_group</h3>
-      <p>Scoring</p>
-      <h4>Aspects to improve:</h4>
-      <div>
-        <p>Skill</p>
-        <p>Skill</p>
-        <p>Skill</p>
+      <div className="rating-title-container">
+        <h3>Skill_group</h3>
+        <p>Scoring</p>
+      </div>
+      <p>Aspects to improve:</p>
+      <div className="rating-skills-container">
+        <p className="rating-skills-item">Skill</p>
+        <p className="rating-skills-item">Skill</p>
+        <p className="rating-skills-item">Skill</p>
       </div>
     </div>
   )
