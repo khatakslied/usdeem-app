1b4b4263d5958ff22ca5a6b0ff5ff46a2750c606
Vivian Bow <vivianbao1009@gmail.com>
2022-02-18 16:55:52 +0900

    Cleaned up codes

--- app/javascript/components/KeyTraitScore.js
+++ app/javascript/components/KeyTraitScore.js
@@ -2,30 +2,6 @@ import React from "react"
 import SkillGroupRating from "./SkillGroupRating"
 
 export default function KeyTraitScore(props) {
-  // const first_report = props.latest_reports[0]
-  // const key_traits = first_report.key_traits.map(key_trait => {
-  //   return(
-  //     <p key={key_trait.category}>{key_trait.category}</p>
-  //   )
-  // }
-  // )
-
-  // const first_key_trait = first_report.key_traits[0]
-  // const skill_groups = first_key_trait.skill_groups.map(skill_group => {
-  //   return (
-  //     <p key={skill_group.category}>{skill_group.category}</p>
-  //   )
-  // }
-  // )
-
-  // const first_skill_group = first_key_trait.skill_groups[0]
-
-  // const skills = first_skill_group.skills.map(skill => {
-  //   return(
-  //     <p key={skill.name}>{skill.name}</p>
-  //   )
-  // })
-
   // target key_trait from each report
   const keyTraitsArray = props.latest_reports.map(report => {
     return(
