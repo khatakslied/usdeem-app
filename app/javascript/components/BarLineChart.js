a24fe38a8d50ac5d5e97f07da67aa76379efc2b0
matias acuna <agroang@gmail.com>
2022-03-27 10:00:48 +0900

    User AllTraitsLineGraph method to get the last 6 months names correctly, tested and working

--- app/javascript/components/BarLineChart.js
+++ app/javascript/components/BarLineChart.js
@@ -21,38 +21,24 @@ ChartJS.register(
   Tooltip
 );
 
-// getLongMonthName = function (date) {
-//   return monthNames[date.getMonth()]
-// }
-// it really doesn't like if I try to use a function, maybe instead I actually
-// need to use state, use effect, and all that kind of stuff, instead of regular
-// functions...
-
 const BarLineChart = props => {
 
-  // need to use props here
-  console.log(props.last_six_months_reports[0].created_at)
-  // Trying to make a function that will return the names of the months from
-  // a date:
+  // console.log(props.all_reports)
 
-  // this should be a state [monthNames, setMonthNames] = React.useState()
-  // the commented out function might be used to call setMonthNames
-  // it should expect an array of props.last_six_months_reports, iterate
-  // through each one and grab the #created_at and call the function using
-  // that, something like getLongMonthName(new Date("1-1-2021")
-  // or in our case getLongMonthName(new Date(report.created_at))
-  // tested on console and getLongMonthName(new Date("2022-03-06T00:00:00.000Z"))
-  // works, but monthNames MUST be there from the beginning, the ones that
-  // I should be setting are the dates that we will use to run the function.
-  // "2022-03-06T00:00:00.000Z". Still need to keep thinking about it.
+  const monthKeysArray = props.last_six_months_reports.map(report => {
+    return new Date(report.created_at).getMonth()
+  }).flat().reverse().slice(-6)
+
   const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
 
+  const monthsArray = monthKeysArray.map(monthKey => {
+    return monthNames[monthKey]
+  });
 
-  // getLongMonthName(props.last_six_months_reports[0].created_at)
-
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+  // const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+  const labels = monthsArray
 
   const options = {
     responsive: true,
