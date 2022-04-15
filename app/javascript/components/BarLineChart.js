b2c162385921ee99b6b19592ccacf3e9d0275207
matias acuna <agroang@gmail.com>
2022-04-15 09:00:14 +0900

    User data is working. Missing company data

--- app/javascript/components/BarLineChart.js
+++ app/javascript/components/BarLineChart.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState, useEffect } from "react"
 import {
   Chart as ChartJS,
   LinearScale,
@@ -22,10 +23,9 @@ ChartJS.register(
 );
 
 const BarLineChart = props => {
+  const [userLastData, setUserLastData] = useState([])
+  // const [CompanyLastData, setCompanyLastData] = useState([])
 
-  // console.log(props.all_reports)
-  console.log(props.last_six_months_reports)
-  // last six months reports are only your reports
   // I need the average of everything per month for the user and for
   // the company
   const monthKeysArray = props.last_six_months_reports.map(report => {
@@ -42,14 +42,13 @@ const BarLineChart = props => {
   });
 
   const labels = monthsArray
+  // console.log(labels, 'Labels')
 
-// ----- MY TRY
+// ----- CURRENT USER DATA
 // Using last_six_months_reports so its the USER scores
 
 // Returns an array with n reports with totalScores(average score in total for
 // that report), and scoresMonth (the month that it was created) as key
-// currently I am missing reports on april...
-// I think the formula for last 6 is working, we just dont have reports for april
   const keyTraitsScoresArray = props.last_six_months_reports.map(report => {
     return { totalScores: report.key_traits.map(traits => {
       return traits.skill_groups.map(skill_group => {
@@ -57,24 +56,62 @@ const BarLineChart = props => {
       }).reduce((p, c) => p + c)
     }).reduce((p, c) => p + c)/report.key_traits.length,
       scoresMonth: monthNames[new Date(report.created_at).getMonth()]
-      // scoresMonth: report.created_at
     }
   });
-// Need to sort by same month and reduce till I have just 6 values in an array
-// so I am looking to get an array back
+  // console.log(keyTraitsScoresArray, 'keyTraitsScoresArray (big one)')
 
-  // console.log(keyTraitsScoresArray)
+// If it exists, to avoid errors, aggregates same months
+// the scores based on how many were the same (month)
+// https://stackoverflow.com/questions/24444738/sum-similar-keys-in-an-array-of-objects
+  if (keyTraitsScoresArray) {
+    const reducedArray = Array.from(keyTraitsScoresArray.reduce((acc, {totalScores, ...r}) => {
+      const key = JSON.stringify(r);
+      const current = acc.get(key) || { ...r, totalScores: 0 };
+      return acc.set(key, { ...current, totalScores: current.totalScores + totalScores });
+    }, new Map).values()).reverse();
+    // console.log(reducedArray, 'reducedArray, aggregated one')
+    // makes an object with the month as keys and the value as the number of times we have that month
+    const counts = {};
+    keyTraitsScoresArray.forEach(function (x) {
+      counts[x.scoresMonth] = (counts[x.scoresMonth] || 0) + 1;
+    });
+    // console.log(counts, 'counts, the number of times the month repeats')
 
-// If it exists, to avoid errors (not working, need to reduce based on the same
-// month!)
-  // if (keyTraitsScoresArray) {
-  //   let testing = Array.from(keyTraitsScoresArray.reduce(
-  //     (m, { scoresMonth, value }) => m.set(scoresMonth, (m.get(scoresMonth) || 0) + value), new Map
-  //   ), ([scoresMonth, value]) => ({ scoresMonth, value }));
-  //   console.log(testing)
-  // }
+    // Divides the totalscores by the months counter and returns a set of
+    // objects with averagedScore and month as keys
+    let finalAveragedMonths = new Set
+    let countsKeys = Object.keys(counts)
 
-  // -------------------- END MY TRY
+    reducedArray.forEach(obj => {
+      if (obj.scoresMonth == countsKeys.find(month => month == obj.scoresMonth)) {
+        finalAveragedMonths.add(
+          { averagedScore: (obj.totalScores / counts[obj.scoresMonth]),
+            month: obj.scoresMonth
+          })
+      }
+    })
+    // console.log(finalAveragedMonths, 'finalAverageMonths, the final set')
+
+    // Creates a new array with the values only in the same order as the labels
+    const finalAverageMonthsArr = Array.from(finalAveragedMonths)
+    // console.log(finalAverageMonthsArr, 'new array from the Set')
+    const dataValues = []
+    finalAverageMonthsArr.forEach(obj => {
+      if (obj.month == labels.find(m => obj.month == m)) {
+        dataValues.push(obj.averagedScore)
+        }
+    })
+    // console.log(dataValues)
+    // Avoids infite loop due to re render
+    useEffect(() => {
+      setUserLastData(dataValues);
+    }, []);
+  }
+  // -------------------- END USER DATA
+
+  // -------------------- COMPANY DATA
+  console.log(props.all_reports)
+  // I need the average of everything for the last 6 months for the company
 
   const options = {
     responsive: true,
@@ -96,8 +133,8 @@ const BarLineChart = props => {
         borderColor: 'rgb(255, 99, 132)',
         borderWidth: 2,
         fill: false,
-        data: [5, 4, 3, 5, 1, 2], // needs to be actual data myRatingData
-      }, // I can just pass an array here
+        data: userLastData
+      },
       {
         type: 'bar',
         label: 'Company Average',
@@ -116,17 +153,3 @@ const BarLineChart = props => {
   )
 };
 export default BarLineChart;
-
-// TESTING
-
-// Saving one of the objects inside the keyTraitsScoresArray
-
-// testingObject = keyTraitsScoresArray[0]
-
-// const sumwithinitial = testingObject.totalScores.reduce(
-//   (previusValue, currentValue) => previusValue + currentValue
-// )
-// => '1,3,13,5,22,4,5,11,1,2,44,2,15,4,5'
-
-// sumwithinitial.split(',').map(num => parseInt(num))
-// => [1, 3, 13, 5, 22, 4, 5, 11, 1, 2, 44, 2, 15, 4, 5]
